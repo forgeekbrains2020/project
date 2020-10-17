754d459e943cfa6799801385b6e02f3922428834
forgeekbrains2020 <forgeekbrains2020@mail.ru>
2020-10-17 20:55:09 +0700

    task_6.py done

--- task_6.py
+++ task_6.py
@@ -25,3 +25,30 @@
 “ед”: [“шт.”]
 }
 """
+products_list = []
+input_repeat = 'yes'
+while input_repeat == 'yes' or input_repeat == 'y':
+    product_num = int(input("Введите номер товара: "))
+    product_name = input("Введите название товара: ")
+    product_cost = float(input("Введите цену товара: "))
+    product_quantity = int(input("Введите количество товара: "))
+    product_unit = input("Введите единицу измерения товара: ")
+    input_repeat = input("Хотите добавить еще товар? Введите (y)es или (n)o: ")
+    products_list.append((product_num, {'название': product_name, 'цена': product_cost, 'количество': product_quantity,
+                                        'ед': product_unit}))
+
+analytics_dict = {}
+product_names_list = []
+product_costs_list = []
+product_quantity_list = []
+product_units_list = []
+
+for item in products_list:
+    product_names_list.append(item[1]['название'])
+    product_costs_list.append(item[1]['цена'])
+    product_quantity_list.append(item[1]['количество'])
+    product_units_list.append(item[1]['ед'])
+analytics_dict = {'названия': product_names_list, 'цены': product_costs_list, 'количества': product_quantity_list,
+                  'ед': product_units_list}
+
+print(analytics_dict)
